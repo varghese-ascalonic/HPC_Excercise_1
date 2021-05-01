a3cf88cfbff4c98cd999c72e85240b8b5945b730
Varghese Mathai <varghesemathaime@gmail.com>
2021-05-01 13:36:56 +0200

    Print max and min

--- automate.py
+++ automate.py
@@ -1,11 +1,17 @@
 #!/usr/bin/python
 
 import sys
+import numpy as np
 from subprocess import Popen, PIPE
 
 times = int(sys.argv[1])
 
+execution_times = np.zeros(times)
+
 for i in range(times):
     process = Popen(["./a.out", "100", "4"], stdout=PIPE)
     (output, err) = process.communicate()
-    print(output)
+    execution_times[i] = int(output)
+
+print("Minimum : " + np.amin(execution_times))
+print("Maximum : " + np.amax(execution_times))
