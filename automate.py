03a1ae40ff9a77aa8a736ab652c616b60d95e61a
chanelno19 <chanelno19@protonmail.com>
2021-05-01 17:11:30 +0200

    n, s as optional args

--- automate.py
+++ automate.py
@@ -5,11 +5,19 @@ import numpy as np
 from subprocess import Popen, PIPE
 
 times = int(sys.argv[1])
+n = '100'
+s = '4'
+
+try:
+    n = sys.argv[2]
+    s = sys.argv[3]
+except IndexError:
+    pass
 
 execution_times = np.zeros(times)
 
 for i in range(times):
-    process = Popen(["./a.out", "100", "4"], stdout=PIPE)
+    process = Popen(["./a.out", n, s], stdout=PIPE)
     (output, err) = process.communicate()
     execution_times[i] = int(output)
 
